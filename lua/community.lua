ab26d2e3be525b010fe8373db32e972da4101af6
Ray Tran <ray@artran.co.uk>
2025-06-05 13:21:52 +0200

    Add noice.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,7 @@ return {
   "AstroNvim/astrocommunity",
 
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  -- { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
