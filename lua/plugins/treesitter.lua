abd94cdfb983e55df5d5d69f18a1697063a121b1
Ray Tran <ray@artran.co.uk>
2025-08-07 16:24:31 +0100

    Set nicer key bindings for treesitter incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,5 +9,14 @@ return {
       "vim",
       -- add more arguments for adding more treesitter parsers
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        scope_incremental = "grn", -- set to `false` to disable one of the mappings
+        node_decremental = "<TAB>",
+      },
+    },
   },
 }
