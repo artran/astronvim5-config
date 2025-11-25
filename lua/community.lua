d23fb2e0394355cf0d0bd1d6367f06802184b035
Ray Tran <rt@elixirsoftware.co.uk>
2025-11-25 16:03:57 +0000

    Remove avante

--- lua/community.lua
+++ lua/community.lua
@@ -8,7 +8,7 @@ return {
 
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.utility.noice-nvim" },
-  { import = "astrocommunity.completion.avante-nvim" },
+  -- { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
